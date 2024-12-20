d05f6fce455bdd0c1ff86905602a8e3b5e7e866f
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-12-21 00:14:56 +0500

    framer motion added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from 'react-router';
 import Footer from './components/Footer';
-import Header from './components/Header';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen justify-between bg-gray-100">
       <Navbar />
-      {/* <Header /> */}
-
       <main className="mb-auto pt-20 ">
         <div className="max-w-xl mx-auto">
           <Outlet />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,82 @@
 import { FaFacebookMessenger, FaWhatsapp } from 'react-icons/fa';
 import { sofaData } from '../lib/contants';
-import { SofaType } from '../lib/types';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const cardVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: 'easeInOut' },
+  },
+  exit: {
+    opacity: 0,
+    y: -50,
+    transition: { duration: 0.8, ease: 'easeInOut' },
+  },
+  hover: {
+    scale: 1.01,
+    transition: { type: 'spring', stiffness: 300 }, // Spring effect
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.15)',
+  },
+};
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.5 },
+  },
+};
 
 function Home() {
   return (
-    <div className=" space-y-6 p-6">
-      {sofaData.map((val: SofaType) => (
-        <div
-          key={val.id}
-          className=" bg-white hover:cursor-pointer rounded-lg shadow-sm hover:shadow-md transition-all border border-slate-200 border-opacity-0 hover:border-opacity-100 overflow-hidden flex items-center"
-        >
-          <div className="w-[40%]">
-            <img
-              src={val.image}
-              alt={val.name}
-              className="w-full h-36 sm:h-48 object-cover"
-            />
-          </div>
-          <div className="w-[60%] py-3 px-4 sm:py-6 sm:px-8">
-            <h2 className="text-lg sm:text-xl font-semibold text-gray-800 mb-1">
-              {val.name}
-            </h2>
-            <p className="text-gray-700 text-base sm:text-lg font-bold mb-3">
-              ${val.price}
-            </p>
-            <div className="flex items-center justify-between">
-              <p className="text-sm text-gray-500">Place Order From</p>
-              <div className="flex space-x-[7px] sm:space-x-3">
-                {/* <button className="rounded-full border border-gray-300 w-9 h-9 flex items-center justify-center text-gray-600 hover:bg-gray-200 transition duration-200">
-                  <span className="text-sm font-medium">F</span>
-                </button> */}
-                <FaFacebookMessenger size={20} />
-                <FaWhatsapp size={20} />
-                {/* <button className="rounded-full border border-gray-300 w-9 h-9 flex items-center justify-center text-gray-600 hover:bg-gray-200 transition duration-200">
-                  <span className="text-sm font-medium">W</span>
-                </button> */}
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="space-y-6 p-6"
+    >
+      <AnimatePresence>
+        {sofaData.map((val) => (
+          <motion.div
+            key={val.id}
+            variants={cardVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            whileHover="hover" // Apply hover animation
+            className="bg-white hover:cursor-pointer rounded-lg shadow-sm hover:shadow-md border border-slate-200 border-opacity-0 hover:border-opacity-100 overflow-hidden flex items-center"
+          >
+            <motion.div whileHover="hover" className="w-[40%]">
+              <img
+                src={val.image}
+                alt={val.name}
+                className="w-full h-36 sm:h-48 object-cover"
+              />
+            </motion.div>
+            <div className="w-[60%] py-3 px-4 sm:py-6 sm:px-8">
+              <h2 className="text-lg sm:text-xl font-semibold text-gray-800 mb-1">
+                {val.name}
+              </h2>
+              <p className="text-gray-700 text-base sm:text-lg font-bold mb-3">
+                ${val.price}
+              </p>
+              <div className="flex items-center justify-between">
+                <p className="text-sm text-gray-500">Place Order From</p>
+                <div className="flex space-x-[7px] sm:space-x-3">
+                  <FaFacebookMessenger size={20} />
+                  <FaWhatsapp size={20} />
+                  {/* <button className="rounded-full border border-gray-300 w-9 h-9 flex items-center justify-center text-gray-600 hover:bg-gray-200 transition duration-200">
+              <span className="text-sm font-medium">W</span>
+            </button> */}
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      ))}
-    </div>
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </motion.div>
   );
 }
 
